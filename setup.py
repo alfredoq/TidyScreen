8fba5611b81ab93d7b0b5e9584f1ac9d40855e5e
Alfredo Quevedo <aquevedo@unc.edu.ar>
2026-03-17 13:46:07 -0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='tidyscreen',
         'py3Dmol',
 	'sqlite_web'
       ],
-      packages=['tidyscreen', 'tidyscreen.chemspace', 'tidyscreen.config', 'tidyscreen.databases', 'tidyscreen.projects', 'tidyscreen.moldock', 'tidyscreen.misc', 'tidyscreen.moldyn', 'tidyscreen.molecule_management'],
+      packages=['tidyscreen', 'tidyscreen.chemspace', 'tidyscreen.config', 'tidyscreen.databases', 'tidyscreen.projects', 'tidyscreen.moldock', 'tidyscreen.misc', 'tidyscreen.moldyn', 'tidyscreen.molecule_management', 'tidyscreen.config'],
       keywords='drug-discovery',
       classifiers=[
           "Programming Language :: Python :: 3.10",
